3c1bc4eb8aec527419fa61ef58a65393750e0997
Laura Pham <l.pham2311@gmail.com>
2022-04-19 21:44:25 +1000

    create renderlink function to refactor Links component code

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,19 +3,23 @@ import ClassicLink from './links/ClassicLink';
 import ShowsLink from './links/ShowsLink';
 import MusicLink from './links/MusicLink';
 
+const renderLink = (type, link) => {
+	if (type === 'shows') {
+		return <ShowsLink {...link} />;
+	}
+	if (type === 'music') {
+		return <MusicLink {...link} />;
+	}
+	return <ClassicLink {...link} />;
+};
+
 // iterates through the users links and shows the correct button according to the link type
 const Links = ({ links }) => {
 	return (
 		<div className='links-container'>
 			{links.map((link) => {
 				const { type } = link;
-				if (type === 'shows') {
-					return <ShowsLink {...link} />;
-				}
-				if (type === 'music') {
-					return <MusicLink {...link} />;
-				}
-				return <ClassicLink {...link} />;
+				return <div key={link.id}>{renderLink(type, link)}</div>;
 			})}
 		</div>
 	);
